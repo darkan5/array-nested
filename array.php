091e860e0e4c0af85a9b7fde38e163606794289e
Tomasz Wiśniewski <tomaszwisniewski@MacBook-Pro.local>
2020-12-31 15:43:46 +0100

    array file

---
+++ array.php
@@ -0,0 +1,21 @@
+<?php
+$table = [] ;
+$table[0]["id"] = 1 ;
+$table[0]["name"] = "p1-1" ;
+$table[0]["id"] = 5 ;
+$table[0]["parent_id"] = 0 ;
+$table[0]["buyed"] = "false" ;
+$table[0]["folderChecked"] = "false" ;
+$table[0]["checked"] = "false" ;
+$table[0]["children"] = [];
+$table[0]["children"] [0]["id"] = 2 ;
+$table[0]["children"] [0]["name"] = "p2-1" ;
+$table[0]["children"] [0]["id"] = 1 ;
+$table[0]["children"] [0]["parent_id"] = 0 ;
+$table[0]["children"] [0]["folderChecked"] = "false" ;
+$table[0]["children"] [0]["buyed"] = "false" ;
+$table[0]["children"] [0]["checked"] = "false" ;
+
+
+
+print_r($table);
